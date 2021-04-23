c7baad42e22805abe241db87efad940412e11245
Mateus Souza <mateus_souza_teles@hotmail.com>
2021-04-22 21:18:01 -0300

    Create component Header
    Create the header that will be contained on all pages of the application.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import { Header } from "../components/Header";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Hello World!</h1>
+    </>
+  );
 }
