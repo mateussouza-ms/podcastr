01a8a831860117ac02ac5bc3d4bbfabbc4a837ac
Mateus Souza <mateus_souza_teles@hotmail.com>
2021-04-23 21:46:59 -0300

    Add axios
    Add axios dependecy to make calls to the APIs.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home(props) {
+interface Episode {
+  id: string;
+  title: string;
+  members: string;
+}
+
+interface HomeProps {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -9,9 +20,14 @@ export default function Home(props) {
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("/episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
 
   return {
     props: {
@@ -19,4 +35,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
