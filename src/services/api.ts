d49bd854a11fcb01b6b2c43766272c6778f9ffb6
Mateus Souza <mateus_souza_teles@hotmail.com>
2021-05-07 21:18:12 -0300

    Configure URL of APIs
    Configure baseURL from Axios for deploy in the Vercel.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const url =
+  process.env.VERCEL == "1"
+    ? "https://podcastr-server.vercel.app/"
+    : "http://localhost:3333/";
+
 export const api = axios.create({
-    baseURL: "http://localhost:3333/",
+  baseURL: url,
 });
